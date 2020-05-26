8cb1664eb4c024c39052f4019550730dba23239b
于凡 <490301640@qq.com>
2020-05-26 10:52:49 +0800

    [Fix]添加字体文件

--- src/pages/Login/loginIndex.js
+++ src/pages/Login/loginIndex.js
@@ -1,152 +1,334 @@
-import React, {Component} from 'react';
-import {Alert, StyleSheet, View, Dimensions, Image, Text, TextInput, TouchableOpacity} from 'react-native'
-import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
-import {Input, Button} from "react-native-elements";
-import Toast, {DURATION} from 'react-native-easy-toast'
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    View,
+    TouchableOpacity,
+    Dimensions,
+    Keyboard,
+} from 'react-native'
+import { size, isIPhoneXPaddTop,isIPhoneXFooter } from '../../../src/Common/tool/ScreenUtil';
 
-export default class LoginIndex extends Component {
+import { postFetch } from '../../../src/Common/network/request/HttpUtils';
 
-    static navigationOptions = {
-        header: null,
-    };
+import {Icon} from 'react-native-elements';
+import color from '../../../Theme';
+import Toast, { DURATION } from 'react-native-easy-toast'
 
+const width = Dimensions.get('window').width
+import { Header, Text, Input, Button } from 'react-native-elements'
+
+export default class Login extends Component {
     constructor(props) {
-        super(props);
-        this._Submission = this._Submission.bind(this);
+        super(props)
         this.state = {
             username: '',
             password: '',
-            keep_password: false,
-            img_eye_close: true,//设置默认显示眼睛关闭的图标
-            is_show_pwd: true, //设置是否明文显示密码
-            secureText: true,
+            name_error: '',
+            pass_error: '',
+            isClick_Bnt: true,
+            isMessage: false,
+            isShowPass: false,
+            time_lab: '获取验证码'
         }
+        this.name_input = React.createRef()
+        this.pass_input = React.createRef()
+        this._onClickLogin = this._onClickLogin.bind(this) //登录
+        this._getPhoneCode = this._getPhoneCode.bind(this) //获取验证码
     }
-
 
     render() {
         return (
-            <View style={styles.container} behavior="padding">
+            <TouchableOpacity
+                style={styles.container}
+                onPress={() => {
+                    Keyboard.dismiss()
+                }}
+                activeOpacity={1}
+            >
+                <Header
+                    backgroundColor={'#FFFFFF'}
+                    leftComponent={
+                        <TouchableOpacity
+                            style={{
+                                width: 40,
+                                height: 40,
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}
+                            onPress={() => {
+                                this.props.navigation.goBack()
+                            }}
+                        >
+                            <Icon name={'guanbi'} size={17} color={color.title} />
+                        </TouchableOpacity>
+                    }
+                    rightComponent={
+                        <Text
+                            style={{ fontSize: 17, color: color.title }}
+                            onPress={() =>
+                                this.props.navigation.push('RegisterAndReset', { type: 1 })
+                            }
+                        >
+                            注册
+                        </Text>
+                    }
+                />
+                <Text
+                    h4
+                    style={{
+                        marginTop: 10,
+                        textAlign: 'center',
+                        color: color.title
+                    }}
+                >
+                    {this.state.isMessage ? '短信登录' : '账号登录'}
+                </Text>
 
-                <View style={styles.login_view}>
+                <View style={styles.center_view}>
                     <Input
-                        ref='username'
-                        shake={true}
-                        placeholder='手机号/邮箱'
-                        leftIconContainerStyle={{marginRight: 20}}
-                        errorMessage={''}
-                        clearButtonMode={'while-editing'}
-                        autoCapitalize='none' // 取消首字母大写
-                        autoComplete={'password'}
-                        returnKeyType={"next"}
-                        onChangeText={(text) => this.setState({username: text})}
-                    />
-                    <Input
-                        containerStyle={{marginTop: 20}}
-                        ref='password'
-                        shake={true}
-                        placeholder='密码'
-                        rightIcon={
-                            <TouchableOpacity onPress={() => this.setState({secureText: !this.state.secureText})}>
-                                <FontAwesomeIcon name={this.state.secureText ? 'eye' : 'eye-slash'} size={22}
-                                                 color='#555555'/>
-                            </TouchableOpacity>
-
+                        ref={this.name_input}
+                        placeholder={
+                            this.state.isMessage ? '短信登录仅限中国大陆用户' : '手机号/邮箱'
                         }
-                        secureTextEntry={this.state.secureText}
-                        leftIconContainerStyle={{marginRight: 20}}
-                        errorMessage={'密码由字母、符号和数字组成'}
-                        // clearButtonMode={'while-editing'}
-                        autoCapitalize='none' // 取消首字母大写
-                        autoComplete={'password'}
-                        onChangeText={(text) => this.setState({password: text})}
+                        leftIcon={
+                            this.state.isMessage ? (
+                                <Text
+                                    style={{ color: color.title, fontSize: 17, marginRight: 20 }}
+                                >
+                                    +86
+                                </Text>
+                            ) : null
+                        }
+                        value={this.state.username}
+                        errorStyle={{ color: 'red' }}
+                        clearButtonMode={'always'}
+                        errorMessage={this.state.name_error}
+                        placeholderTextColor={color.gray}
+                        containerStyle={{ marginBottom: 40 }}
+                        inputStyle={{ fontSize: 17, color: color.title }}
+                        keyboardType={this.state.isMessage ? 'numeric' : 'default'}
+                        onChangeText={text => {
+                            this.setState({ username: text })
+                        }}
+                    />
+                    <Input
+                        ref={this.pass_input}
+                        placeholder={this.state.isMessage ? '请输入验证码' : '密码'}
+                        rightIcon={
+                            this.state.isMessage ? (
+                                <Button
+                                    type={'Clear'}
+                                    titleStyle={{ fontSize: 12 }}
+                                    disabled={this.state.username.length > 10 ? false : true}
+                                    disabledStyle={{ backgroundColor: '#edeeef' }}
+                                    title={this.state.time_lab}
+                                    onPress={this._getPhoneCode}
+                                />
+                            ) : (
+                                <Icon
+                                    name={'yulan'}
+                                    size={30}
+                                    style={{ margin: 10 }}
+                                    color={this.state.isShowPass ? color.title : color.subhead}
+                                    onPress={() => {
+                                        this.setState({ isShowPass: !this.state.isShowPass })
+                                    }}
+                                />
+                            )
+                        }
+                        value={this.state.password}
+                        secureTextEntry={
+                            this.state.isMessage
+                                ? false
+                                : this.state.isShowPass
+                                ? false
+                                : true
+                        }
+                        selectTextOnFocus={true}
+                        clearButtonMode={'always'}
+                        errorStyle={{ color: 'red' }}
+                        errorMessage={this.state.pass_error}
+                        placeholderTextColor={color.gray}
+                        containerStyle={{ marginBottom: 40 }}
+                        inputStyle={{ fontSize: 17, color: color.subhead }}
+                        onChangeText={text => {
+                            this.setState({ password: text })
+                        }}
+                        keyboardType={this.state.isMessage ? 'numeric' : 'default'}
                     />
 
+                    <Button
+                        title={'登录'}
+                        titleStyle={{ color: '#FFFFFF', fontSize: 17 }}
+                        buttonStyle={{
+                            backgroundColor: color.orange,
+                            margin: 10,
+                            height: 55
+                        }}
+                        disabled={
+                            this.state.username.length < 11 && this.state.password.length < 6
+                        }
+                        disabledStyle={{ backgroundColor: color.gray }}
+                        disabledTitleStyle={{ color: '#FFFFFF', fontSize: 17 }}
+                        onPress={this._onClickLogin}
+                    />
+
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            marginLeft: 0,
+                            marginRight: 0,
+                            justifyContent: 'space-between'
+                        }}
+                    >
+                        <Button
+                            type="clear"
+                            title={this.state.isMessage ? '账号密码登录' : '手机短信登录'}
+                            titleStyle={{ color: color.subhead, fontSize: 17 }}
+                            onPress={() => {
+                                this.setState({ isMessage: !this.state.isMessage })
+                            }}
+                        />
+                        <Button
+                            type="clear"
+                            title={'忘记密码'}
+                            titleStyle={{ color: color.subhead, fontSize: 17 }}
+                            onPress={() => {
+                                this.props.navigation.push('RegisterAndReset', { type: 2 })
+                            }}
+                        />
+                    </View>
                 </View>
-                <Button buttonStyle={{marginTop: 40}}
-                        title="登录"
-                        onPress={() => this._Submission()}
-                />
-                <Button buttonStyle={{marginTop: 40}}
-                        title="注册"
-                        onPress={() => this.props.navigation.push('Register')}
-                    // onPress={() => this._zhuce()}
 
-                />
-
-                <Toast ref="toast" position='center'/>
-
-            </View>
-        );
+                <Text
+                    style={{
+                        marginTop: size(180),
+                        textAlign: 'center',
+                        color: color.title,
+                        fontSize: 16,
+                        height: 30
+                    }}
+                >
+                    第三方账号登录
+                </Text>
+                <View style={styles.bottom_icon}>
+                    <Icon
+                        name={'weixin1'}
+                        size={38}
+                        onPress={() => {
+                            this._otherClick(0)
+                        }}
+                    />
+                    <Icon
+                        name={'weibo1'}
+                        size={38}
+                        onPress={() => {
+                            this._otherClick(1)
+                        }}
+                    />
+                    <Icon
+                        name={'qq'}
+                        size={38}
+                        onPress={() => {
+                            this._otherClick(2)
+                        }}
+                    />
+                </View>
+                <Toast ref="toast" position="center" />
+            </TouchableOpacity>
+        )
     }
 
-    _Submission() {
-
-        let data = {
-            'account': this.state.username,
-            'password': this.state.password
-        };
-
-        // HttpsUtils.post(Tools.USER_PATH('/api/login?backUrl=http://uc.test'), data)
-        //     .then(result => {
-        //         if (result.message) {
-        //             this.refs.toast.show(result.message, 500);
-        //             return;
-        //         }
-        //
-        //         DeviceStorage.save('login_back', result, function () {
-        //
-        //         });
-        //         this._getUserInfo({'ticket': result.ticket});
-        //         // DeviceStorage.get('login_back').then((username) => {
-        //         //     alert(JSON.stringify(username));
-        //         // });
-        //
-        //     })
-        //     .catch(error => {
-        //         this.refs.toast.show(error, 500);
-        //     })
+    _onClickLogin = () => {
+        //登录
     }
 
-    _getUserInfo(data) {
+    _otherClick(num) {
 
-        // HttpsUtils.post(Tools.GET_PATH('/api/auth/login'), data)
-        //     .then(result => {
-        //         if (result.message) {
-        //             this.refs.toast.show(result.message, 500);
-        //             return;
-        //         }
-        //         DeviceStorage.save('user_info', result, function () {
-        //
-        //         });
-        //
-        //         this.refs.toast.show('成功，正在录入信息...', 500, () => {
-        //             this.props.navigation.navigate('App');
-        //             this.props.navigation.goBack();
-        //         })
-        //
-        //     })
-        //     .catch(error => {
-        //         this.refs.toast.show(error, 500);
-        //     })
+        let HH = '';
+        switch (num) {
+            case 0: //微信
+                HH = "微信";
+                break
+            case 1: //微博
+                HH = "微博";
+                break
+            case 2: //QQ
+                HH = "QQ";
+                break
+        }
+
+        alert(HH);
+
     }
 
+    _getPhoneCode = () => {
+        //获取验证码
+        if (this.state.time > 0) {
+            return
+        }
 
+        postFetch(
+            '/api/register/get_verify_code',
+            { account: this.state.username },
+            response => {
+                if (response && response.data.length > 0) {
+                    if (response.status === 200) {
+                        this.refs.toast.show('验证码发送成功，请注意接收!', 500)
+                        this.countTime(60)
+                    } else {
+                        alert(JSON.stringify(response))
+                    }
+                } else {
+                    this.refs.toast.show('获取验证码失败', 500)
+                }
+            }
+        )
+    }
+
+    countTime(num) {
+        let new_time = num
+        this._timer = setInterval(() => {
+            new_time--
+            this.setState({ time: new_time })
+            if (this.state.time <= 0) {
+                this.setState({
+                    time: new_time,
+                    time_lab: '获取验证码'
+                })
+                this._timer && clearInterval(this._timer)
+            } else {
+                this.setState({
+                    time: new_time,
+                    time_lab: `重新获取(${new_time}s)`
+                })
+            }
+        }, num * 10)
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
-        backgroundColor: 'white',//'#F4F4F4',
+        flexDirection: 'column',
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'center'
     },
-    login_view: {
-        marginTop: 90,
-        padding: 20,
-        justifyContent: 'center',
-        // backgroundColor: 'white',
-        // borderRadius: 6,
-        // borderColor: "#efecea",
-        // borderWidth: 1
+    center_view: {
+        marginTop: size(30),
+        marginLeft: 0,
+        marginRight: 0,
+        padding: 30
     },
-});
+    bottom_icon: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginLeft: 60,
+        marginRight: 60,
+        marginBottom: isIPhoneXFooter(40),
+        height: size(50)
+    }
+})
