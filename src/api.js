0f544963370c05291295ddb73ffadbf35848114b
Liu Fei <peterfeispace@gmail.com>
2020-05-26 14:17:51 +0800

    [Refactor]重构api接口

--- src/api.js
+++ src/api.js
@@ -1,21 +1,22 @@
 import React, {Component} from 'react';
 //dev 开发   prod 产品
-
-let active = 'dev';
-
-let API_URL;
-let GET_COURSES;
-//正式
-if (active == 'prod') {
-    API_URL = 'http://dev.nc.com';
-}
-if (active == 'test' || active == 'dev') {
-    //开发和测试
-    API_URL = 'http://dev.nc.com';
+const host = {
+  dev: {
+      API_URL: 'http://nc_course.dev.test:88',
+  },
+  prd: {
+      API_URL: 'https://nc.course.com',
+  }
 }
 
-export default class api extends Component {
-    static API_URL = API_URL;
-    static GET_COURSES = '/api/excellent_courses/courses?'; //精品课列表
-    static GET_CATEGORIES = '/api/categories'; //课程分类列表
+let ENV = 'dev'
+let currentHost = host[ENV]
+
+const setHost = (env = 'dev') => {
+  ENV = env
+  currentHost = host[ENV]
 }
+const API_URL = currentHost.API_URL
+
+
+export {ENV, API_URL,  setHost}
